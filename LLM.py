3b3f1f5572e8fa1c187da12de95b982c06ef334c
turtneck <90732829+turtneck@users.noreply.github.com>
2026-04-21 01:05:30 -0400

    train_BERT_model inital implementation

--- LLM.py
+++ LLM.py
@@ -2,7 +2,9 @@
 #   container for LLM class(es)
 from transformers import BertTokenizer, BertModel
 import torch
+from dataloader import dataloaderLANG
 from jeb382private import Bertize #private file
+
 
 
 #helper module
@@ -67,8 +69,93 @@ class LLMBERT_Model():
     
     
 #==============================================================================
-def train_BERT_model(betterbert,epoch,lr,trainingtestingsplit):
-    pass
+import numpy as np
+import time
+from sklearn.metrics import roc_auc_score, accuracy_score, classification_report
+import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+
+
+
+#helper func
+def getAUC(y_true, y_score):
+    try: return roc_auc_score(y_true, y_score, multi_class='ovr')
+    except ValueError: return roc_auc_score(y_true, y_score[:, 1])  
+
+
+def train_BERT_model(betterbert,epoch,lr,datasplit):
+    data = dataloaderLANG(datasplit)
+    X_train, y_train = data.TrainX, data.TrainY
+    X_val, y_val = data.ValX, data.ValY
+    
+    
+    
+    #===============
+    #   training
+    #===============
+    model = LLMBERT_Model(betterbert=betterbert)
+    optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-4)
+    criterion = lambda outputs, targets: focal_loss(outputs, targets, alpha=0.25, gamma=2.0)# 0.05 8.0 gave best at 0.83 AUC
+    scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2) #was 10 and 2
+    
+    print('epoching')
+    for current_epoch in range(epoch):
+        stopwatch = time.monotonic()    #jeb382
+        #----------
+        model.train()
+        running_loss = 0.0
+        y_true = []
+        y_pred = []
+
+        for input, labels in zip(X_train,y_train):
+            optimizer.zero_grad()
+            
+            # Forward pass
+            outputs = model(input)
+            # Calculate loss
+            loss = criterion(outputs, labels)
+            # Backward pass and optimization
+            loss.backward()
+            optimizer.step()
+            # Accumulate loss
+            running_loss += loss.item()
+            
+            # Store true and predicted labels for AUC computation
+            y_true.extend(labels.cpu().numpy())
+            y_pred.extend(torch.softmax(outputs, dim=1).detach().cpu().numpy())
+
+        # Compute AUC
+        auc_train = getAUC(np.array(y_true), np.array(y_pred))
+        train_loss = running_loss / len(X_train)
+        #----------
+        stopwatch=time.monotonic()-stopwatch
+        print(  f"Epoch [{current_epoch + 1}/{epoch}]  -  Train Loss: {train_loss:.5f}  -  Train AUC: {auc_train:.5f}  -  {(stopwatch/60):.4f}m")    #jeb382
+        scheduler.step()
+    
+    
+    
+    #===============
+    #   validation
+    #===============
+    model.eval()
+    y_true = []
+    y_pred = []
+    with torch.no_grad():
+        for input, labels in zip(X_val,y_val):
+            outputs = model(input)
+            y_true.extend(labels.cpu().numpy())
+            y_pred.extend(torch.softmax(outputs, dim=1).detach().cpu().numpy())
+    y_true=np.array(y_true)
+    y_pred=np.array(y_pred)
+    
+    
+    
+    #===============
+    #   metrics
+    #===============
+    print(f"MODEL: BERT   -   better?={betterbert}")
+    print("Accuracy:", accuracy_score(y_val, y_pred))
+    print(classification_report(y_val, y_pred, target_names=data.encoder.classes_))
     
     
     
